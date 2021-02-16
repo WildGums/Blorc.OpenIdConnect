6da68512b9d955ad2bac33734b104128b781d4d8
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-02-15 20:31:10 -0500

    Remove useless using

--- src/Blorc.OpenIdConnect/Services/TokenBasedSecuredHttpClient.cs
+++ src/Blorc.OpenIdConnect/Services/TokenBasedSecuredHttpClient.cs
@@ -3,6 +3,7 @@
     using System;
     using System.IO;
     using System.Net.Http;
+    using System.Net.Http.Json;
     using System.Text.Json;
     using System.Threading;
     using System.Threading.Tasks;
