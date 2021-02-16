c4e12abb781b4d6ab7f6e54f48f808ce8007d807
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-02-15 20:32:13 -0500

    Optimize using

--- src/Blorc.OpenIdConnect/Services/TokenBasedSecuredHttpClient.cs
+++ src/Blorc.OpenIdConnect/Services/TokenBasedSecuredHttpClient.cs
@@ -3,7 +3,6 @@
     using System;
     using System.IO;
     using System.Net.Http;
-    using System.Net.Http.Json;
     using System.Text.Json;
     using System.Threading;
     using System.Threading.Tasks;
