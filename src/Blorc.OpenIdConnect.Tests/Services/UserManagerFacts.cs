19dca7d6a85f578148acea537090245f93bb9a66
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2022-06-20 15:21:43 -0400

    Fixes dependencies

--- src/Blorc.OpenIdConnect.Tests/Services/UserManagerFacts.cs
+++ src/Blorc.OpenIdConnect.Tests/Services/UserManagerFacts.cs
@@ -1,87 +1,87 @@
-﻿namespace Blorc.OpenIdConnect.Tests.Services;
-
-using System;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Blorc.OpenIdConnect.DemoApp;
-using Blorc.OpenIdConnect.Models;
-using Blorc.Services;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
-using Moq;
-using NUnit.Framework;
-using Is = NUnit.DeepObjectCompare.Is;
-
-[TestFixture]
-public class UserManagerFacts
+﻿namespace Blorc.OpenIdConnect.Tests.Services
 {
+    using System;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using Blorc.OpenIdConnect.Models;
+    using Blorc.Services;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.JSInterop;
+    using Moq;
+    using NUnit.Framework;
+    using Is = NUnit.DeepObjectCompare.Is;
+
     [TestFixture]
-    public class The_GetUserAsync_Generic_Method
+    public class UserManagerFacts
     {
-        [Test]
-        public async Task Returns_An_Instance_Of_The_Specified_User_Type_Async()
+        [TestFixture]
+        public class The_GetUserAsync_Generic_Method
         {
-            var jsRuntimeMock = new Mock<IJSRuntime>();
-            jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<bool>("BlorcOidc.Client.UserManager.IsAuthenticated", Array.Empty<object>())).ReturnsAsync(true);
-
-            var jsonElement = JsonSerializer.Deserialize<JsonElement>("{\r\n  \"access_token\": \"1234567890\"\r\n}");
-
-            jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<JsonElement>("BlorcOidc.Client.UserManager.GetUser", Array.Empty<object>())).ReturnsAsync(jsonElement);
-
-            var oidcProviderOptions = new OidcProviderOptions();
-
-            var navigationManagerStub = new Stub<NavigationManager>(new NavigationManagerStub());
-            navigationManagerStub.SetField("_isInitialized", true);
-            navigationManagerStub.SetField("_uri", "http://localhost");
-
-            var userManager = new UserManager(jsRuntimeMock.Object, navigationManagerStub.Instance, new Mock<IConfigurationService>().Object, oidcProviderOptions);
-
-            var user = await userManager.GetUserAsync<User<Profile>>();
-
-            Assert.IsNotNull(user);
-        }
-
-        [Test]
-        public async Task Returns_An_Instance_Of_The_Specified_User_Type_With_The_Expected_Values_In_All_Properties_Async()
-        {
-            var jsRuntimeMock = new Mock<IJSRuntime>();
-            jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<bool>("BlorcOidc.Client.UserManager.IsAuthenticated", Array.Empty<object>())).ReturnsAsync(true);
-
-            var expectedUser = new User<Profile>
+            [Test]
+            public async Task Returns_An_Instance_Of_The_Specified_User_Type_Async()
             {
-                AccessToken = "1234567890",
-                Profile = new Profile
-                {
-                    Roles = new[] { "Administrator", "System Administrator" },
-                    Email = "jane.doe@blorc.com",
-                    EmailVerified = true,
-                    FamilyName = "Doe",
-                    GivenName = "Jane",
-                    Name = "Jane Doe",
-                    PreferredUsername = "jane.doe"
-                },
-                ExpiresAt = 10,
-                SessionState = "alskjdhflaskjdhflaksjdhqwpoyir",
-                TokenType = "Bearer"
-            };
+                var jsRuntimeMock = new Mock<IJSRuntime>();
+                jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<bool>("BlorcOidc.Client.UserManager.IsAuthenticated", Array.Empty<object>())).ReturnsAsync(true);
 
-            var serializedExpectedUser = JsonSerializer.Serialize(expectedUser);
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>("{\r\n  \"access_token\": \"1234567890\"\r\n}");
 
-            var jsonElement = JsonSerializer.Deserialize<JsonElement>(serializedExpectedUser);
+                jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<JsonElement>("BlorcOidc.Client.UserManager.GetUser", Array.Empty<object>())).ReturnsAsync(jsonElement);
 
-            jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<JsonElement>("BlorcOidc.Client.UserManager.GetUser", Array.Empty<object>())).ReturnsAsync(jsonElement);
+                var oidcProviderOptions = new OidcProviderOptions();
 
-            var oidcProviderOptions = new OidcProviderOptions();
+                var navigationManagerStub = new Stub<NavigationManager>(new NavigationManagerStub());
+                navigationManagerStub.SetField("_isInitialized", true);
+                navigationManagerStub.SetField("_uri", "http://localhost");
 
-            var navigationManagerStub = new Stub<NavigationManager>(new NavigationManagerStub());
-            navigationManagerStub.SetField("_isInitialized", true);
-            navigationManagerStub.SetField("_uri", "http://localhost");
+                var userManager = new UserManager(jsRuntimeMock.Object, navigationManagerStub.Instance, new Mock<IConfigurationService>().Object, oidcProviderOptions);
 
-            var userManager = new UserManager(jsRuntimeMock.Object, navigationManagerStub.Instance, new Mock<IConfigurationService>().Object, oidcProviderOptions);
+                var user = await userManager.GetUserAsync<User<Profile>>();
 
-            var user = await userManager.GetUserAsync<User<Profile>>();
+                Assert.IsNotNull(user);
+            }
 
-            Assert.That(user, Is.DeepEqualTo(expectedUser));
+            [Test]
+            public async Task Returns_An_Instance_Of_The_Specified_User_Type_With_The_Expected_Values_In_All_Properties_Async()
+            {
+                var jsRuntimeMock = new Mock<IJSRuntime>();
+                jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<bool>("BlorcOidc.Client.UserManager.IsAuthenticated", Array.Empty<object>())).ReturnsAsync(true);
+
+                var expectedUser = new User<Profile>
+                                   {
+                                       AccessToken = "1234567890",
+                                       Profile = new Profile
+                                                 {
+                                                     Roles = new[] { "Administrator", "System Administrator" },
+                                                     Email = "jane.doe@blorc.com",
+                                                     EmailVerified = true,
+                                                     FamilyName = "Doe",
+                                                     GivenName = "Jane",
+                                                     Name = "Jane Doe",
+                                                     PreferredUsername = "jane.doe"
+                                                 },
+                                       ExpiresAt = 10,
+                                       SessionState = "alskjdhflaskjdhflaksjdhqwpoyir",
+                                       TokenType = "Bearer"
+                                   };
+
+                var serializedExpectedUser = JsonSerializer.Serialize(expectedUser);
+
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>(serializedExpectedUser);
+
+                jsRuntimeMock.Setup(runtime => runtime.InvokeAsync<JsonElement>("BlorcOidc.Client.UserManager.GetUser", Array.Empty<object>())).ReturnsAsync(jsonElement);
+
+                var oidcProviderOptions = new OidcProviderOptions();
+
+                var navigationManagerStub = new Stub<NavigationManager>(new NavigationManagerStub());
+                navigationManagerStub.SetField("_isInitialized", true);
+                navigationManagerStub.SetField("_uri", "http://localhost");
+
+                var userManager = new UserManager(jsRuntimeMock.Object, navigationManagerStub.Instance, new Mock<IConfigurationService>().Object, oidcProviderOptions);
+
+                var user = await userManager.GetUserAsync<User<Profile>>();
+
+                Assert.That(user, Is.DeepEqualTo(expectedUser));
+            }
         }
     }
 }
