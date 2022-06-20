1c8cea7c45a87c0406393a010754ae61ec8c6209
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2022-06-20 10:47:21 -0400

    Improves ToObject JsonElement extension method

--- src/Blorc.OpenIdConnect/Services/JsonElementExtensions.cs
+++ src/Blorc.OpenIdConnect/Services/JsonElementExtensions.cs
@@ -1,13 +1,19 @@
 ﻿namespace Blorc.OpenIdConnect
 {
+    using System.Buffers;
     using System.Text.Json;
 
     public static class JsonElementExtensions
     {
         public static TObject ToObject<TObject>(this JsonElement element, JsonSerializerOptions options = null)
         {
-            var rawText = element.GetRawText();
-            return JsonSerializer.Deserialize<TObject>(rawText, options);
+            var bufferWriter = new ArrayBufferWriter<byte>();
+            using (var writer = new Utf8JsonWriter(bufferWriter))
+            {
+                element.WriteTo(writer);
+            }
+
+            return JsonSerializer.Deserialize<TObject>(bufferWriter.WrittenSpan, options);
         }
     }
 }
