620b6e1bf61dc30db85f4f15f515665f2ac91549
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2022-06-22 09:46:54 -0400

    Removes Catel.Linq using in UserManagerFacts

--- src/Blorc.OpenIdConnect.Tests/Services/UserManagerFacts.cs
+++ src/Blorc.OpenIdConnect.Tests/Services/UserManagerFacts.cs
@@ -5,7 +5,6 @@
     using System.Threading.Tasks;
     using Blorc.OpenIdConnect;
     using Blorc.Services;
-    using Catel.Linq;
     using Microsoft.AspNetCore.Components;
     using Microsoft.JSInterop;
     using Moq;
