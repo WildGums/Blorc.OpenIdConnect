8f302cb3b8fc2588bddd00c23e047516162e3af7
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2022-06-22 09:45:43 -0400

    Fixes members order in ClaimTypeAttribute

--- src/Blorc.OpenIdConnect/Attributes/ClaimTypeAttribute.cs
+++ src/Blorc.OpenIdConnect/Attributes/ClaimTypeAttribute.cs
@@ -4,11 +4,11 @@
 
     public class ClaimTypeAttribute : Attribute
     {
-        public string ClaimType { get; }
-
         public ClaimTypeAttribute(string claimType)
         {
             ClaimType = claimType;
         }
+
+        public string ClaimType { get; }
     }
 }
