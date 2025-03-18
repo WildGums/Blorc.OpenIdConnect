594c868a9c8c8fc09622fcbf3d993c76670264cc
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2025-03-18 12:16:29 -0500

    Adds unit test to verify SignOutRedirectAsync is not triggered when token is not expired

--- src/Blorc.OpenIdConnect.Tests/Services/AccessTokenExpirationDelegatingHandlerFacts.cs
+++ src/Blorc.OpenIdConnect.Tests/Services/AccessTokenExpirationDelegatingHandlerFacts.cs
@@ -62,7 +62,49 @@
                 userManagerMock.Verify(um => um.SignOutRedirectAsync(), Times.Once);
             }
 
-            private class TestableAccessTokenExpirationDelegatingHandler : AccessTokenExpirationDelegatingHandler
+            [Test]
+            public async Task Does_Not_Call_SignOutRedirectAsync_When_The_Token_Is_Not_Expired_Async()
+            {
+                // Arrange
+                var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+                using var httpResponseMessage = new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("Fake response")
+                };
+
+                mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                    .ReturnsAsync(httpResponseMessage);
+
+                var userManagerMock = new Mock<IUserManager>();
+                var now = DateTimeOffset.UtcNow;
+                var user = new User<Profile>
+                {
+                    ExpiresAt = now.AddSeconds(10).ToUnixTimeSeconds() 
+                };
+
+                userManagerMock.Setup(userManager => userManager.GetUserAsync<User<Profile>>(It.IsAny<bool>(), It.IsAny<JsonSerializerOptions>())).ReturnsAsync(user);
+
+                var fakeTimeProvider = new FakeTimeProvider();
+                fakeTimeProvider.SetUtcNow(now);
+
+                using var handler = new TestableAccessTokenExpirationDelegatingHandler(userManagerMock.Object, fakeTimeProvider);
+                handler.InnerHandler = mockHttpMessageHandler.Object;
+
+                using var request = new HttpRequestMessage(HttpMethod.Get, "https://example.com");
+
+                // Act
+                await handler.SendAsync(request, CancellationToken.None);
+
+                // Assert
+                userManagerMock.Verify(um => um.SignOutRedirectAsync(), Times.Never);
+            }
+
+            private sealed class TestableAccessTokenExpirationDelegatingHandler : AccessTokenExpirationDelegatingHandler
             {
                 public TestableAccessTokenExpirationDelegatingHandler(IUserManager userManager, TimeProvider timeProvider)
                     : base(userManager, timeProvider) { }
